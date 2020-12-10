14473899c9aa1cb81b0498c0f2bb7f54380b979c
kroim <wonderfulplayer@outlook.com>
2020-12-10 09:33:36 +0800

    Change API endpoints URL

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-const API_BASE_URL = 'http://localhost:4000/api'
+// const API_BASE_URL = 'http://localhost:4000/api'
+const API_BASE_URL = '/api'
 export const postCall = (data, successCallback, failCallback = null) => {
     let token = sessionStorage.getItem('token');
     let headers = { 'Content-Type': 'application/graphql' };
