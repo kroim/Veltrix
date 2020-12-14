b969545792943f0bef88176738a0b0a84d0a66d7
kzar1102 <kzar1102@gmail.com>
2020-12-15 00:35:02 +0800

    mailer setting

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     mongodb_user_passwd: '',
     secret: "veltrix-secret-key",
     mailer_host: 'smtp.gmail.com',
-    mailer_port: 587,
+    mailer_port: 465,
     mailer_user: 'sarkis19930919@gmail.com',
     mailer_pass: 'w"gq(Ej6QVfB7.)$u'
 }
