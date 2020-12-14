af95328d3de52eedd479c155c345adebda63150e
kroim <wonderfulplayer@outlook.com>
2020-12-14 21:30:25 +0800

    File Formatter

--- server/config.js
+++ server/config.js
@@ -1,14 +1,12 @@
-
 module.exports = {
-    mongodb_host : 'localhost',
-    mongodb_port : '27017',
+    mongodb_host: 'localhost',
+    mongodb_port: '27017',
     mongodb_database: 'veltrixdb',
-    mongodb_user_name : '',
-    mongodb_user_passwd : '',
+    mongodb_user_name: '',
+    mongodb_user_passwd: '',
     secret: "veltrix-secret-key",
     mailer_host: 'smtp.gmail.com',
     mailer_port: 587,
     mailer_user: '',
     mailer_pass: ''
 }
-
